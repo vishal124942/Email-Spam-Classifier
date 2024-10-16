d494e10673fd8d5f7aba963bcebe3c8aa95c73c3
vishal verma <vishalverma4942@gmail.com>
2024-10-16 18:11:52 +0530

    Added CORS support to app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 from spam_classifier import predict_mail  
 import json
 app = Flask(__name__)
+CORS(app)
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
@@ -28,5 +30,6 @@ def predict():
     except Exception as e:
         # General error handling
         return jsonify({"error": str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
